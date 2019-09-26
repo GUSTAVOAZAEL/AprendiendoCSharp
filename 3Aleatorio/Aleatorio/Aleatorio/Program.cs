0119e9558c96d257aa8badd386f8a9765d6e99c5
GUSTAVOAZAEL <gustavoazaael@users.noreply.github.com>
2019-09-25 21:59:17 -0500

    Programa 3 listo

---
+++ 3Aleatorio/Aleatorio/Aleatorio/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aleatorio
+{
+    class Program
+    {
+        static public float numX = 50F;  //variable de clase
+        
+        static void Main(string[] args)
+        {
+            float numY = 0F;            //variable local
+            Random numAle = new Random();
+            numY = (float)numAle.Next(1, 11);
+
+            Console.WriteLine(String.Format("La suma de {0} y {1} es {2}", numX, numY, numX + numY));
+            Console.ReadLine();
+
+
+        }
+    }
+}
