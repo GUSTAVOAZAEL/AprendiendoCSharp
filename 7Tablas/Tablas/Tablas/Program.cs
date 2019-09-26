0c8ac1408dc404c4500c828b14b473ec7af6150b
GUSTAVOAZAEL <gustavoazaael@gmail.com>
2019-09-25 22:13:11 -0500

    Programa 7 listo

---
+++ 7Tablas/Tablas/Tablas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tablas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("");
+                Console.WriteLine(String.Format("Tabla del {0}: ", i));
+                Console.WriteLine();
+
+                for (int o = 1; o <= 10; o++)
+                {
+                    Console.WriteLine(String.Format("{0} x {1} = {2}", i, o, i * o));
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Preciona una tecla...");
+            Console.ReadKey();
+        }
+    }
+}
