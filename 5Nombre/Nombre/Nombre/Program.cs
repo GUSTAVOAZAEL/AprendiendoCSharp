6c2182ae3d5a81f70532a065e48bd75b1fe3a88f
GUSTAVOAZAEL <gustavoazaael@gmail.com>
2019-09-25 22:09:09 -0500

    Programa 5 listo

---
+++ 5Nombre/Nombre/Nombre/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom;
+            string aps;
+
+            Console.Write("Escribe un nombre:  ");
+            nom = Console.ReadLine();
+
+            Console.Write("Escribe los apellidos:  ");
+            aps = Console.ReadLine();
+
+            nom = nom.ToUpper();
+            aps = aps.ToUpper();
+
+            StringBuilder nomCompl = new StringBuilder(nom);
+            nomCompl.Append("  ");
+            nomCompl.Append(aps);
+
+            Console.Write(nomCompl);
+
+            Console.WriteLine("  ");
+            Console.Write("Preciona una tecla...");
+            Console.ReadKey();
+
+
+        }
+    }
+}
