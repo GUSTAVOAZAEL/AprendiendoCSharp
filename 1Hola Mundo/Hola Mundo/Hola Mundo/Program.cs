4fffbc657abcf4e1eaae5f8f08813b2f8b05c57e
GUSTAVOAZAEL <gustavoazaael@users.noreply.github.com>
2019-09-25 21:06:12 -0500

    Programa 1 listo

---
+++ 1Hola Mundo/Hola Mundo/Hola Mundo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hola_Mundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo! Este es mi primer actividad para el repositorio! ;D ");
+            Console.ReadLine();
+        }
+    }
+}
