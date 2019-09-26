8b438ed8b11d47785a63786feaba92591fd4f58d
GUSTAVOAZAEL <gustavoazaael@gmail.com>
2019-09-25 22:16:30 -0500

    Programa 10 listo

---
+++ 10Multiplo/Multiplo/Multiplo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _num;
+            int num;
+            bool esMul3, esMul5, esMul7;
+
+            try
+            {
+                Console.Write("Dame un numero entero:  ");
+                _num = Console.ReadLine();
+                num = Convert.ToInt32(_num);
+
+
+                esMul3 = ((num % 3) == 0);
+                esMul5 = ((num % 5) == 0);
+                esMul7 = ((num % 7) == 0);
+
+                if ((esMul3 & esMul5) | esMul7)
+                {
+                    Console.WriteLine("Correcto.");
+                }
+                else
+                {
+                    Console.WriteLine("Incorrecto.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("El dato proporcionado causa errores.");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Preciona INTRO...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
