bb0d0e8b94f0df1309138e9065de02ed76734310
GUSTAVOAZAEL <gustavoazaael@users.noreply.github.com>
2019-09-25 21:55:26 -0500

    Segundo programa listo

---
+++ 2Conversiones/Conversiones/Conversiones/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversiones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = "1234";
+            Console.WriteLine(num.GetType().ToString());
+
+            int num1 = Convert.ToInt32(num);
+            Console.WriteLine(num1.GetType().ToString());
+
+            Double prueba = Convert.ToDouble(num1);
+            Console.WriteLine(prueba.GetType().ToString());
+
+
+
+            Console.ReadLine();
+        
+
+        }
+    }
+}
