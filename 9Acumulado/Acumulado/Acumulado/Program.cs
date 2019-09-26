8520fe8b54780642fbee1ad6b9e37a7e0bbee748
GUSTAVOAZAEL <gustavoazaael@gmail.com>
2019-09-25 22:15:32 -0500

    Programa 9 listo

---
+++ 9Acumulado/Acumulado/Acumulado/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acumulado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _num;
+            int num;
+            int acum = 0;
+
+            Console.WriteLine("Introduzca los enteros a acumular.");
+            Console.WriteLine("Para salir, solo dejar vacio y precionar INTRO.");
+            Console.WriteLine("");
+
+            while (true)
+            {
+                Console.Write("Dame un numero entero: ");
+                _num = Console.ReadLine();
+                if (_num == "")
+                {
+                    break;
+                }
+                else
+                {
+                    if (int.TryParse(_num, out num))
+                    {
+                        acum += num;
+                        Console.WriteLine(String.Format("Monto acumulado: {0}", acum));
+                    }
+                    else
+                    {
+                        Console.WriteLine("El dato proporcionado no es un numero entero.");
+                        Console.WriteLine("Intenta de nuevo.");
+                    }
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Preciona una INTRO para salir.");
+            Console.ReadKey();
+        }
+    }
+}
